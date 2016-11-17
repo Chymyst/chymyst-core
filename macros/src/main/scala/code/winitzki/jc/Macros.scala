ababbccaa75df33e61ae94e36845235492690281
Sergei Winitzki <sergei@versal.com>
2016-11-16 22:39:37 -0800

    WIP towards inspecting reactions via macro

--- macros/src/main/scala/code/winitzki/jc/Macros.scala
+++ macros/src/main/scala/code/winitzki/jc/Macros.scala
@@ -1,5 +1,6 @@
 package code.winitzki.jc
 
+import scala.collection.mutable
 import scala.language.experimental.macros
 import scala.reflect.macros._
 import scala.reflect.NameTransformer.LOCAL_SUFFIX_STRING
@@ -47,11 +48,119 @@ object Macros {
     c.Expr[JS[T, R]](q"new JS[$t,$r](Some($s))")
   }
 
+  sealed trait PatternFlag
+    case object Wildcard extends PatternFlag
+    case object SimpleVar extends PatternFlag
+    case object SimpleConst extends PatternFlag
+    case object OtherPattern extends PatternFlag
+
   def findInputs(arg: UnapplyArg => Unit): String = macro findInputsImpl
 
   def findInputsImpl(c: theContext)(arg: c.Expr[UnapplyArg => Unit]) = {
     import c.universe._
-    q"${show(arg)}"
+
+    // example tree, somewhat trimmed. More precisely, we have a List() of CaseDef's.
+    /*
+    val _ = CaseDef(
+          UnApply(
+            Apply(Select(Select(Ident("code.winitzki.jc.JoinRun"), "code.winitzki.jc.JoinRun.$plus"), TermName("unapply")),
+              List(Ident(TermName("<unapply-selector>")))
+            ),
+            List(UnApply(
+              Apply(Select(Select(Ident("code.winitzki.jc.JoinRun"), "code.winitzki.jc.JoinRun.$plus"), TermName("unapply")),
+                List(Ident(TermName("<unapply-selector>")))
+              ),
+              List(UnApply(Apply(Select(Select(Ident("code.winitzki.jc.JoinRun"), "code.winitzki.jc.JoinRun.$plus"), TermName("unapply")),
+                List(Ident(TermName("<unapply-selector>")))
+              ),
+                List(UnApply(Apply(Select(Select(Ident("code.winitzki.jc.JoinRun"), "code.winitzki.jc.JoinRun.$plus"), TermName("unapply")),
+                  List(Ident(TermName("<unapply-selector>")))
+                ),
+                  List(UnApply(Apply(Select(Select(Ident("code.winitzki.jc.JoinRun"), "code.winitzki.jc.JoinRun.$plus"), TermName("unapply")),
+                    List(Ident(TermName("<unapply-selector>")))
+                  ),
+                    List(UnApply(Apply(Select(Select(Ident("code.winitzki.jc.JoinRun"), "code.winitzki.jc.JoinRun.$plus"), TermName("unapply")),
+                      List(Ident(TermName("<unapply-selector>")))
+                    ),
+                      List(UnApply(Apply(Select(Select(Ident("code.winitzki.jc.JoinRun"), "code.winitzki.jc.JoinRun.$plus"), TermName("unapply")),
+                        List(Ident(TermName("<unapply-selector>")))
+                      ),
+                        List(UnApply(Apply(
+                          Select(Ident(TermName("a")), TermName("unapply")),
+                          List(Ident(TermName("<unapply-selector>")))
+                        ),
+                          List(Bind(TermName("p"), Ident(termNames.WILDCARD)))
+                        ),
+                          UnApply(Apply(
+                            Select(Ident(TermName("a")), TermName("unapply")),
+                            List(Ident(TermName("<unapply-selector>")))
+                          ),
+                            List(Bind(TermName("y"), Ident(termNames.WILDCARD)))
+                          )
+                        )
+                      ),
+                        UnApply(Apply(
+                          Select(Ident(TermName("a")), TermName("unapply")),
+                          List(Ident(TermName("<unapply-selector>")))
+                        ),
+                          List(Literal(Constant(1)))
+                        )
+                      )
+                    ),
+                      UnApply(Apply(Select(Ident(TermName("b")), TermName("unapply")),
+                        List(Ident(TermName("<unapply-selector>")))
+                      ),
+                        List(Ident(termNames.WILDCARD))
+                      )
+                    )
+                  ),
+                    UnApply(Apply(Select(Ident(TermName("b")), TermName("unapply")),
+                      List(Ident(TermName("<unapply-selector>")))
+                    ),
+                      List(Apply(TypeTree().setOriginal(Select(Ident("scala"), "scala.Tuple2")),
+                          List(Literal(Constant(1)), Bind(TermName("z"), Ident(termNames.WILDCARD)))
+                        )
+                      )
+                    )
+                  )
+                ),
+                  UnApply(Apply(Select(Ident(TermName("b")), TermName("unapply")), List(Ident(TermName("<unapply-selector>")))), List(Apply(TypeTree().setOriginal(Select(Ident("scala"), "scala.Tuple2")), List(Ident(termNames.WILDCARD), Select(Ident("scala"), "scala.None"))))))), UnApply(Apply(Select(Ident(TermName("b")), TermName("unapply")), List(Ident(TermName("<unapply-selector>")))), List(Apply(TypeTree().setOriginal(Select(Ident("scala"), "scala.Tuple2")), List(Bind(TermName("t"), Ident(termNames.WILDCARD)), Apply(TypeTree().setOriginal(Select(Ident("scala"), "scala.Some")), List(Bind(TermName("q"), Ident(termNames.WILDCARD)))))))))), UnApply(Apply(Select(Ident(TermName("s")), TermName("unapply")), List(Ident(TermName("<unapply-selector>")))), List(Ident(termNames.WILDCARD), Bind(TermName("r"), Ident(termNames.WILDCARD)))))), EmptyTree, Apply(Select(Apply(Select(Ident("code.winitzki.jc.JoinRun"), TermName("JoinableUnit")), List(Apply(Select(Ident(TermName("a")), TermName("apply")), List(Apply(Select(Ident(TermName("p")), TermName("$plus")), List(Literal(Constant(1)))))))), TermName("$plus")), List(Apply(Select(Ident(TermName("r")), TermName("apply")), List(Ident(TermName("p")))))))
+
+*/
+
+    object GatherInfo extends Traverser {
+      var info: mutable.ArrayBuffer[(Any, PatternFlag)] = mutable.ArrayBuffer()
+
+      def getFlag(binderTerm: Tree): PatternFlag = binderTerm match {
+        case Ident(termNames.WILDCARD) => Wildcard
+        case Bind(TermName(_), Ident(termNames.WILDCARD)) => SimpleVar
+        case Literal(_) => SimpleConst
+        case _ => OtherPattern
+      }
+      // TODO: gather info about all "apply" operations originating from molecules or reply actions
+      // TODO: filter by type signature of t, check consistency of the type of t vs. one or two binders used
+      override def traverse(tree: c.universe.Tree): Unit = {
+        tree match {
+          case UnApply(Apply(Select(t@Ident(TermName(name)), TermName("unapply")), List(Ident(TermName("<unapply-selector>")))), List(binder)) =>
+            info.append((t.symbol.typeSignature, getFlag(binder)))
+
+          case UnApply(Apply(Select(t@Ident(TermName(name)), TermName("unapply")), List(Ident(TermName("<unapply-selector>")))), List(binder1, binder2)) =>
+            info.append((t.symbol.typeSignature, getFlag(binder1)))
+
+          //          case Apply(Select(t@Ident(TermName(name)), TermName("unapply")), List(Ident(TermName("<unapply-selector>")))) =>
+//            info.append((t.symbol.typeSignature, OtherPattern))
+
+          case _ => super.traverse(tree)
+        }
+      }
+    }
+//    c.abort(c.enclosingPosition, "")
+    // determine which variables are captured by closure?
+    // determine types of symbols that use .apply
+    GatherInfo.traverse(arg.tree)
+    println(s"Gathered info: ${GatherInfo.info}")
+    val s = showRaw(arg)
+    q"$s"
   }
 
 }
