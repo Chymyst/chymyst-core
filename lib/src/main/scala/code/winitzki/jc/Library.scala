23285d02110109f9528fdfc61c45bd4bb2f75f4e
sergei.winitzki <sergei.winitzki@workday.com>
2016-11-17 18:54:10 -0800

    better functionality for future / molecule interface

--- lib/src/main/scala/code/winitzki/jc/Library.scala
+++ lib/src/main/scala/code/winitzki/jc/Library.scala
@@ -2,11 +2,16 @@ package code.winitzki.jc
 
 import code.winitzki.jc.JoinRun._
 
-import scala.concurrent.{Future, Promise}
+import scala.concurrent.{ExecutionContext, Future, Promise}
 import scala.reflect.ClassTag
 
 object Library {
-  def makeFuture[T : ClassTag]: (JA[T], Future[T]) = {
+  /** Create an output molecule that, when injected, will resolve the future.
+    *
+    * @tparam T Type of value carried by the molecule and by the future.
+    * @return Tuple of molecule injector and the future
+    */
+  def moleculeFuture[T : ClassTag]: (JA[T], Future[T]) = {
     val f = ja[T]
     val p = Promise[T]()
 
@@ -14,6 +19,31 @@ object Library {
       & { case f(x) => p.success(x) }
     )
     (f, p.future)
+  }
+
+  implicit class FutureWithMolecule[T](f: Future[T])(implicit ec: ExecutionContext) {
+    /** Modify the future: when it succeeds, it will additionally inject a given molecule.
+      * The value on the molecule will be equal to the result value of the future.
+      * (The result value of the future is unchanged.)
+      *
+      * @param m Molecule injector, must have the same type as the future.
+      * @return The modified future.
+      */
+    def &(m: JA[T]): Future[T] = f map { x =>
+      m(x)
+      x
+    }
+
+    /** Modify the future: when it succeeds, it will additionally inject a given molecule.
+      * The molecule will carry the specified value (the result value of the future is unchanged).
+      *
+      * @param u Molecule injection expression, such as a(123)
+      * @return The modified future.
+      */
+    def +(u: => Unit): Future[T] = f map { x =>
+      u
+      x
+    }
   }
 
 }
