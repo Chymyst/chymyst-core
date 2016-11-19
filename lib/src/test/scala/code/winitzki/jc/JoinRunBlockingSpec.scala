81de67b0f2a338ed650cb9be1dac8496fd73a247
Sergei Winitzki <sergei@versal.com>
2016-11-18 18:48:18 -0800

    add new blocking tests

---
+++ lib/src/test/scala/code/winitzki/jc/JoinRunBlockingSpec.scala
@@ -0,0 +1,307 @@
+package code.winitzki.jc
+
+import JoinRun._
+import org.scalatest.concurrent.TimeLimitedTests
+import scala.concurrent.blocking
+import org.scalatest.time.{Millis, Span}
+import org.scalatest.{FlatSpec, Matchers}
+
+/** More unit tests for blocking molecule functionality.
+  *
+  */
+class JoinRunBlockingSpec extends FlatSpec with Matchers with TimeLimitedTests {
+
+  val timeLimit = Span(500, Millis)
+
+  val warmupTimeMs = 50
+
+  def waitSome(): Unit = Thread.sleep(warmupTimeMs)
+
+
+  it should "block for a synchronous molecule" in {
+
+    val a = ja[Unit]
+    val f = js[Unit,Int]
+    join( &{ case a(_) + f(_, r) => r(3) })
+    a()
+    a()
+    a()
+    f() shouldEqual 3
+    f() shouldEqual 3
+    f() shouldEqual 3
+  }
+
+  it should "not timeout when a synchronous molecule is responding" in {
+
+    val a = ja[Unit]
+    val f = js[Unit,Int]
+    join( &{ case a(_) + f(_, r) => r(3) })
+    a()
+    f(timeout = 100000000)() shouldEqual Some(3)
+  }
+
+  it should "timeout when a synchronous molecule is not responding at all" in {
+
+    val a = ja[Unit]
+    val f = js[Unit,Int]
+    join( &{ case a(_) + f(_, r) => r(3) })
+    a()
+    f() shouldEqual 3 // now the a() molecule is gone
+    f(timeout = 100000000)() shouldEqual None
+  }
+
+  it should "not timeout when a synchronous molecule is responding quickly enough" in {
+
+    val a = ja[Unit]
+    val f = js[Unit,Int]
+    join( &{ case a(_) + f(_, r) => Thread.sleep(50); r(3) })
+    a()
+    f(timeout = 100000000)() shouldEqual Some(3)
+  }
+
+  it should "timeout when a synchronous molecule is not responding quickly enough" in {
+
+    val a = ja[Unit]
+    val f = js[Unit,Int]
+    join( &{ case a(_) + f(_, r) => Thread.sleep(150); r(3) })
+    a()
+    f(timeout = 100000000)() shouldEqual None
+  }
+
+
+  it should "throw exception when a reaction attempts to reply twice" in {
+    val c = ja[Int]("c")
+    val g = js[Unit,Int]("g")
+    join(
+      &{ case c(n) + g(_,r) => c(n) + r(n) + r(n+1) }
+    )
+    c(2)
+    waitSome()
+
+    val thrown = intercept[Exception] {
+      println(s"got result: ${g()} but should not have printed this!")
+    }
+    thrown.getMessage shouldEqual "Error: In Join{c + g/S => ...}: Reaction {c + g/S => ...} replied to g/S more than once"
+  }
+
+  it should "throw exception when a reaction attempts to reply twice to more than one molecule" in {
+    val c = ja[Int]("c")
+    val d = ja[Unit]("d")
+    val g = js[Unit,Int]("g")
+    val g2 = js[Unit,Int]("g2")
+    join(
+      &{ case d(_) => g2() },
+      &{ case c(n) + g(_,r) + g2(_, r2) => c(n) + r(n) + r(n+1) + r2(n) + r2(n+1) }
+    )
+    c(2) + d()
+    waitSome()
+
+    val thrown = intercept[Exception] {
+      println(s"got result: ${g()} but should not have printed this!")
+    }
+    thrown.getMessage shouldEqual "Error: In Join{d => ...; c + g/S + g2/S => ...}: Reaction {c + g/S + g2/S => ...} replied to g/S, g2/S more than once"
+  }
+
+  it should "throw exception when a reaction does not reply to one blocking molecule" in {
+    val c = ja[Unit]("c")
+    val g = js[Unit,Int]("g")
+    join(
+      &{ case c(_) + g(_,r) => c() }
+    )
+    c()
+    waitSome()
+
+    val thrown = intercept[Exception] {
+      println(s"got result: ${g()} but should not have printed this!")
+    }
+    thrown.getMessage shouldEqual "Error: In Join{c + g/S => ...}: Reaction {c + g/S => ...} finished without replying to g/S"
+  }
+
+  it should "throw exception when a reaction does not reply to two blocking molecules)" in {
+    val c = ja[Unit]("c")
+    val d = ja[Unit]("d")
+    val g = js[Unit,Int]("g")
+    val g2 = js[Unit,Int]("g2")
+    val tp = new ReactionPool(2)
+    join(
+      &{ case d(_) => g2() } onThreads tp,
+      &{ case c(_) + g(_,_) + g2(_,_) => c() }
+    )
+    c() + d()
+    waitSome()
+
+    val thrown = intercept[Exception] {
+      println(s"got result2: ${g()} but should not have printed this!")
+    }
+    thrown.getMessage shouldEqual "Error: In Join{d => ...; c + g/S + g2/S => ...}: Reaction {c + g/S + g2/S => ...} finished without replying to g/S, g2/S"
+
+    tp.shutdownNow()
+  }
+
+  it should "throw exception when a reaction does not reply to one blocking molecule but does reply to another" in {
+    val c = ja[Unit]("c")
+    val d = ja[Unit]("d")
+    val g = js[Unit,Int]("g")
+    val g2 = js[Unit,Int]("g2")
+    val tp = new ReactionPool(2)
+    join(
+      &{ case d(_) => g2() } onThreads tp,
+      &{ case c(_) + g(_,r) + g2(_,_) => c() + r(0) }
+    )
+    c() + d()
+    waitSome()
+
+    val thrown = intercept[Exception] {
+      println(s"got result: ${g()} but should not have printed this!")
+    }
+    thrown.getMessage shouldEqual "Error: In Join{d => ...; c + g/S + g2/S => ...}: Reaction {c + g/S + g2/S => ...} finished without replying to g2/S"
+
+    tp.shutdownNow()
+  }
+
+  it should "not produce deadlock when two blocking molecules are injected from different threads" in {
+    val c = ja[Unit]("c")
+    val d = ja[Unit]("d")
+    val e = ja[Int]("e")
+    val f = ja[Unit]("f")
+    val g = js[Unit,Int]("g")
+    val g2 = js[Unit,Int]("g2")
+    val h = js[Unit,Int]("h")
+    val tp = new ReactionPool(2)
+    join(
+      &{ case c(_) => e(g2()) } onThreads tp, // e(0) should be injected now
+      &{ case d(_) + g(_,r) + g2(_,r2) => r(0) + r2(0) } onThreads tp,
+      &{ case e(x) + h(_,r) =>  r(x) }
+    )
+    c()+d()
+    waitSome()
+    g() shouldEqual 0
+    // now we should also have e(0)
+    h() shouldEqual 0
+
+    tp.shutdownNow()
+
+  }
+
+  it should "produce deadlock when two blocking molecules are injected from the same thread" in {
+    val c = ja[Unit]("c")
+    val d = ja[Unit]("d")
+    val e = ja[Int]("e")
+    val f = ja[Unit]("f")
+    val g = js[Unit,Int]("g")
+    val g2 = js[Unit,Int]("g2")
+    val h = js[Unit,Int]("h")
+    val tp = new ReactionPool(2)
+    join(
+      &{ case c(_) => val x = g() + g2(); e(x) } onThreads tp, // e(0) should never be injected because this thread is deadlocked
+      &{ case d(_) + g(_,r) + g2(_,r2) => r(0) + r2(0) } onThreads tp,
+      &{ case e(x) + h(_,r) =>  r(x) },
+      &{ case d(_) + f(_) => e(2) },
+      &{ case f(_) + e(_) => e(1) }
+    )
+    d()
+    waitSome()
+    c()
+    waitSome()
+    // if e(0) exists now, it will react with f() and produce e(1)
+    f()
+    waitSome()
+    // if e(0) did not appear, f() is still available and will now react with d and produce e(2)
+    d()
+    waitSome()
+    h() shouldEqual 2
+
+    tp.shutdownNow()
+  }
+
+  it should "not block the entire threadpool when one thread is blocked" in {
+    val c = ja[Unit]("c")
+    val d = ja[Unit]("d")
+    val g = js[Unit,Int]("g")
+    val g2 = js[Unit,Int]("g2")
+    val tp = new ReactionPool(2)
+    join(
+      &{ case d(_) => g() } onThreads tp, // this will be used to inject g() and blocked
+      &{ case c(_) + g(_,r) => r(0) } onThreads tp, // this will not start because we have no c()
+      &{ case g2(_, r) => r(1) } onThreads tp // we will use this to test whether the entire thread pool is blocked
+    )
+    g2() shouldEqual 1 // this should initially work
+    d() // do not inject c(). Now the first reaction is blocked because second reaction cannot start.
+    g2() shouldEqual 1 // this should continue to work
+    tp.shutdownNow()
+  }
+
+  it should "block the entire threadpool when one thread is blocked" in {
+    val c = ja[Unit]("c")
+    val d = ja[Unit]("d")
+    val g = js[Unit,Int]("g")
+    val g2 = js[Unit,Int]("g2")
+    val tp = new ReactionPool(1)
+    join(
+      &{ case d(_) => g() } onThreads tp, // this will be used to inject g() and block one thread
+      &{ case c(_) + g(_,r) => r(0) } onThreads tp, // this will not start because we have no c()
+      &{ case g2(_, r) => r(1) } onThreads tp // we will use this to test whether the entire thread pool is blocked
+    )
+    g2() shouldEqual 1 // this should initially work
+    d() // do not inject c(). Now the first reaction is blocked because second reaction cannot start.
+    g2(timeout = 100000000)() shouldEqual None // this should be blocked now
+    tp.shutdownNow()
+  }
+
+  it should "block the fixed threadpool when one thread is sleeping with Thread.sleep" in {
+    val d = ja[Unit]("d")
+    val g2 = js[Unit,Int]("g2")
+    val tp = new ReactionPool(1)
+    join(
+      &{ case d(_) => Thread.sleep(500) } onThreads tp, // this thread is blocked by sleeping
+      &{ case g2(_, r) => r(1) } onThreads tp // we will use this to test whether the entire thread pool is blocked
+    )
+    g2() shouldEqual 1 // this should initially work
+    d() // Now the first reaction is sleeping.
+    g2(timeout = 1000000L*150)() shouldEqual None // this should be blocked now
+    tp.shutdownNow()
+  }
+
+  it should "block the fixed threadpool when one thread is sleeping with blocking(Thread.sleep)" in {
+    val d = ja[Unit]("d")
+    val g2 = js[Unit,Int]("g2")
+    val tp = new ReactionPool(1)
+    join(
+      &{ case d(_) => blocking {Thread.sleep(500)} } onThreads tp, // this thread is blocked by sleeping
+      &{ case g2(_, r) => r(1) } onThreads tp // we will use this to test whether the entire thread pool is blocked
+    )
+    g2() shouldEqual 1 // this should initially work
+    d() // Now the first reaction is sleeping.
+    g2(timeout = 1000000L*150)() shouldEqual None // this should be blocked now
+    tp.shutdownNow()
+  }
+
+  it should "block the cached threadpool when one thread is sleeping with Thread.sleep" in {
+    val d = ja[Unit]("d")
+    val g2 = js[Unit,Int]("g2")
+    val tp = new CachedReactionPool(1)
+    join(
+      &{ case d(_) => Thread.sleep(500) } onThreads tp, // this thread is blocked by sleeping
+      &{ case g2(_, r) => r(1) } onThreads tp // we will use this to test whether the entire thread pool is blocked
+    )
+    g2() shouldEqual 1 // this should initially work
+    d() // Now the first reaction is sleeping.
+    g2(timeout = 1000000L*150)() shouldEqual None // this should be blocked now
+    tp.shutdownNow()
+  }
+
+  it should "not block the cached threadpool when one thread is sleeping with blocking(Thread.sleep)" in {
+    val d = ja[Unit]("d")
+    val g2 = js[Unit,Int]("g2")
+    val tp = new CachedReactionPool(1)
+    join(
+      &{ case d(_) => blocking {Thread.sleep(500)} } onThreads tp, // this thread is blocked by sleeping
+      &{ case g2(_, r) => r(1) } onThreads tp // we will use this to test whether the entire thread pool is blocked
+    )
+    g2() shouldEqual 1 // this should initially work
+    d() // Now the first reaction is sleeping.
+    g2(timeout = 1000000L*50)() shouldEqual Some(1) // this should not be blocked now
+    tp.shutdownNow()
+  }
+
+}
