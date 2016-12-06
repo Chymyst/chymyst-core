ff877f5fea7a5ad0d031eab319aaa113e75c396c
sergei.winitzki <sergei.winitzki@workday.com>
2016-12-06 12:26:18 -0800

    WIP adding a failing test, need to debug

--- benchmark/src/test/scala/code/winitzki/benchmark/JoinRunStaticAnalysisSpec.scala
+++ benchmark/src/test/scala/code/winitzki/benchmark/JoinRunStaticAnalysisSpec.scala
@@ -16,16 +16,37 @@ class JoinRunStaticAnalysisSpec extends FlatSpec with Matchers with TimeLimitedT
 
   behavior of "static reaction analysis"
 
-  it should "detect shadowing of reactions" in {
+  it should "detect shadowing of reactions with wildcards" in {
     val thrown = intercept[Exception] {
       val a = m[Unit]
       val b = m[Unit]
       join(
-        & { case a(_) => () },
-        & { case a(_) + b(_) => () }
+        & { case a(_) => },
+        & { case a(_) + b(_) => }
       )
     }
     thrown.getMessage shouldEqual "In Join{a + b => ...; a => ...}: Unavoidable indeterminism: reaction a + b => ... is shadowed by a => ..."
+  }
+
+  it should "detect shadowing of reactions with unfallible matchers" in {
+    val thrown = intercept[Exception] {
+      val a = m[Int]
+      val b = m[Int]
+      join(
+        & { case a(x) => },
+        & { case a(1) + b(2) => }
+      )
+    }
+    thrown.getMessage shouldEqual "In Join{a + b => ...; a => ...}: Unavoidable indeterminism: reaction a + b => ... is shadowed by a => ..."
+  }
+
+  it should "detect no shadowing of reactions with nontrivial matchers" in {
+    val a = m[Int]
+    val b = m[Unit]
+    join(
+      & { case a(1) => },
+      & { case a(_) + b(_) => }
+    ) shouldEqual ()
   }
 
 }
