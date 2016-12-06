9905999f35334eb5eea06d6c90aaad1c60cbca9e
Sergei Winitzki <sergei@versal.com>
2016-12-05 22:32:54 -0800

    rework some intermittently failing tests

--- lib/src/test/scala/code/winitzki/jc/LibrarySpec.scala
+++ lib/src/test/scala/code/winitzki/jc/LibrarySpec.scala
@@ -30,7 +30,7 @@ class LibrarySpec extends FlatSpec with Matchers with TimeLimitedTests {
       runSimple { case c(_) => waiter.dismiss() }
     )
 
-    Future { Thread.sleep(20) } & c    // insert a molecule from the end of the future
+    Future { Thread.sleep(10) } & c    // insert a molecule from the end of the future
 
     waiter.await() // Waiter default is 150ms
     tp.shutdownNow()
