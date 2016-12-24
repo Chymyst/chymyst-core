da576fa11dc4d5c4e166493b3b2484637845a8ae
Phil Derome <phderome@gmail.com>
2016-12-24 17:17:11 -0500

    added Smokers example

--- joinrun/src/test/scala/code/chymyst/test/Patterns01Spec.scala
+++ joinrun/src/test/scala/code/chymyst/test/Patterns01Spec.scala
@@ -51,7 +51,7 @@ class Patterns01Spec extends FlatSpec with Matchers with BeforeAndAfterEach {
     val result: Seq[String] = logFile.iterator().asScala.toSeq
     // now, there must be f1 and f2 (in any order) before g1 and g2 (also in any order)
     result.size shouldEqual 4
-    Set(result(0), result(1)) shouldEqual Set("f1", "f2")
+    Set(result.head, result(1)) shouldEqual Set("f1", "f2")
     Set(result(2), result(3)) shouldEqual Set("g1", "g2")
   }
 
@@ -98,4 +98,74 @@ class Patterns01Spec extends FlatSpec with Matchers with BeforeAndAfterEach {
     result shouldEqual ((456*456,123*123))
   }
 
+  it should "implement smokers" in {
+    val supplyLineSize = 10
+    def waitSome(): Unit = Thread.sleep(math.floor(scala.util.Random.nextDouble*20.0 + 2.0).toLong)
+
+    case class ShippedInventory(tobacco: Int, paper: Int, matches: Int)
+    case class SupplyChainState(inventory: Int, shipped: ShippedInventory)
+
+    val pusher = new M[SupplyChainState]("Pusher has delivered some unit")
+    val KeithsFix = new E("Keith is smoking having obtained tobacco and matches and rolled a cigarette")
+    val SlashsFix = new E("Slash is smoking having obtained tobacco and paper and rolled a cigarette")
+    val JimisFix = new E("Jimi is smoking having obtained matches and paper and rolled a cigarette")
+
+    val KeithInNeed = new E("Keith is in need of tobacco and matches")
+    val SlashInNeed = new E("Slash is in need of tobacco and paper")
+    val JimiInNeed = new E("Jimi is in need of matches and paper")
+
+    val tobaccoShipment = new M[SupplyChainState]("tobacco shipment")
+    val matchesShipment = new M[SupplyChainState]("matches shipment")
+    val paperShipment = new M[SupplyChainState]("paper shipment")
+
+    val pusherDone = new E("done")
+    val check = new EE("check") // blocking Unit, only blocking molecule of the example.
+
+    val logFile = new ConcurrentLinkedQueue[String]
+
+    site(tp) (
+      go { case pusher(SupplyChainState(n, ShippedInventory(t, p, m))) =>
+        logFile.add(s"$n,$t,$p,$m")
+        scala.util.Random.nextInt(3) match { // select the 2 ingredients randomly
+          case 0 =>
+            val s = SupplyChainState(n-1, ShippedInventory(t+1, p, m+1))
+            tobaccoShipment(s)
+            matchesShipment(s)
+          case 1 =>
+            val s = SupplyChainState(n-1, ShippedInventory(t+1, p+1, m))
+            tobaccoShipment(s)
+            paperShipment(s)
+          case _ =>
+            val s = SupplyChainState(n-1, ShippedInventory(t, p+1, m+1))
+            matchesShipment(s)
+            paperShipment(s)
+        }
+        if (n == 1) pusherDone()
+      },
+      go { case KeithsFix(_) => KeithInNeed() },
+      go { case SlashsFix(_) => SlashInNeed() },
+      go { case JimisFix(_) => JimiInNeed() },
+      go { case pusherDone(_) + check(_, r) => r() },
+
+      go { case KeithInNeed(_) + tobaccoShipment(s) + matchesShipment(_) =>
+        KeithsFix(); waitSome(); pusher(s)
+      },
+      go { case SlashInNeed(_) + tobaccoShipment(s) + paperShipment(_) =>
+        SlashsFix(); waitSome(); pusher(s)
+      },
+      go { case JimiInNeed(_) + matchesShipment(s) + paperShipment(_) =>
+        JimisFix(); waitSome(); pusher(s)
+      }
+    )
+
+    KeithInNeed(()) + SlashInNeed(()) + JimiInNeed(())
+    pusher(SupplyChainState(supplyLineSize, ShippedInventory(0,0,0)))
+    check()
+    val result = logFile.iterator().asScala.toSeq
+    (0 until supplyLineSize).foreach { i =>
+        val current: Array[String] = result(i).split(',')
+        List(current(1).toInt, current(2).toInt, current(3).toInt).sum shouldEqual(2*i) // # ingredients handed out at each cycle is twice number of cycles
+        current(0).toInt + i shouldEqual supplyLineSize // # cycles outstanding + cycles ran should be 10.
+    }
+  }
 }
