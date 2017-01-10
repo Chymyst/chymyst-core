be66c2d132f031e9f6d4b223c894f669fb087712
Sergei Winitzki <sergei@versal.com>
2017-01-10 06:46:11 -0800

    add a test with parameterized types

--- macros/src/test/scala/code/chymyst/jc/GuardsSpec.scala
+++ macros/src/test/scala/code/chymyst/jc/GuardsSpec.scala
@@ -24,6 +24,23 @@ class GuardsSpec extends FlatSpec with Matchers {
     result.info.toString shouldEqual "a(None) + a(Some(1)) + bb((2,Some(3))) => a(Some(1))"
   }
 
+  it should "use parameterized types in guard condition" in {
+    val a = m[Option[Int]]
+    val bb = m[(Int,Option[String])]
+
+    val result = go { case a(xOpt) + bb( y ) if xOpt.isEmpty && y._2.isEmpty => }
+    result.info.guardPresence.knownFalse shouldEqual true
+    result.info.guardPresence should matchPattern { case GuardPresent(List(List('xOpt), List('y)),None,List()) => }
+
+    result.info.inputs should matchPattern {
+      case List(
+      InputMoleculeInfo(`a`, SimpleVar('xOpt, Some(_)), _),
+      InputMoleculeInfo(`bb`, SimpleVar('y, Some(_)), _)
+      ) =>
+    }
+    result.info.toString shouldEqual "a(xOpt if ?) + bb(y if ?) => "
+  }
+
   it should "correctly recognize an indentically false guard condition" in {
     val a = m[Int]
     val n = 10
