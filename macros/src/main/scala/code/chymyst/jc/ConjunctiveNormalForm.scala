afb45d1b3c227d0297038eb1a64ceef21ed0cb14
Sergei Winitzki <sergei@versal.com>
2017-01-13 17:50:15 -0800

    move most of the CNF code into a separate file since it’s not macro specific

---
+++ macros/src/main/scala/code/chymyst/jc/ConjunctiveNormalForm.scala
@@ -0,0 +1,24 @@
+package code.chymyst.jc
+
+object ConjunctiveNormalForm {
+  def disjunctionOneTerm[T](a: T, b: List[List[T]]): List[List[T]] = b.map(y => (a :: y).distinct).distinct
+
+  def disjunctionOneClause[T](a: List[T], b: List[List[T]]): List[List[T]] = b.map(y => (a ++ y).distinct).distinct
+
+  def disjunction[T](a: List[List[T]], b: List[List[T]]): List[List[T]] = a.flatMap(x => disjunctionOneClause(x, b)).distinct
+
+  def conjunction[T](a: List[List[T]], b: List[List[T]]): List[List[T]] = (a ++ b).distinct
+
+  def negation[T](negationOneTerm: T => T)(a: List[List[T]]): List[List[T]] = a match {
+    case x :: xs =>
+      val nxs = negation(negationOneTerm)(xs)
+      x.flatMap(t => disjunctionOneTerm(negationOneTerm(t), nxs))
+    case Nil => List(List()) // negation of true is false
+  }
+
+  def trueConstant[T]: List[List[T]] = List()
+
+  def falseConstant[T]: List[List[T]] = List(List())
+
+  def oneTerm[T](a: T): List[List[T]] = List(List(a))
+}
