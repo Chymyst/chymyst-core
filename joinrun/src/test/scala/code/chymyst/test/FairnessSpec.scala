7aba4f85e2421933f6517e9a4cc2a84cbc120215
Sergei Winitzki <sergei@versal.com>
2017-01-15 00:26:05 -0800

    improve a test

--- joinrun/src/test/scala/code/chymyst/test/FairnessSpec.scala
+++ joinrun/src/test/scala/code/chymyst/test/FairnessSpec.scala
@@ -75,9 +75,9 @@ class FairnessSpec extends FlatSpec with Matchers with TimeLimitedTests {
 
   it should "fail to implement fairness across molecules" in {
 
-    val counters = 10
+    val counters = 20
 
-    val cycles = 1000
+    val cycles = 4000
 
     val c = m[Int]
     val done = m[List[Int]]
