5cea97f594b10c4772c1cea93354e82b41855fd9
winitzki <winitzki@gmail.com>
2017-01-18 16:50:58 -0800

    add a project that uses JoinRun from an unmanaged JAR

---
+++ helloworld/src/main/scala/sample/HelloWorldChymyst.scala
@@ -0,0 +1,13 @@
+package sample
+
+import code.chymyst.jc._
+
+object HelloWorldChymyst extends App {
+  // Define a function that starts some reactions, then call it.
+
+  def startReactions(): Unit = {
+    ???
+  }
+
+  startReactions()
+}
