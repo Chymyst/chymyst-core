e0979a67994649d00605514702f7ff444bb491cf
winitzki <winitzki@gmail.com>
2017-01-18 17:55:02 -0800

    add hello-world example

--- helloworld/src/test/scala/sample/HelloWorldChymystSpec.scala
+++ helloworld/src/test/scala/sample/HelloWorldChymystSpec.scala
@@ -7,6 +7,6 @@ class HelloWorldChymystSpec extends FlatSpec with Matchers {
   behavior of "hello world application"
 
   it should "start reactions" in {
-    ???
+    HelloWorldChymyst.startReactions() shouldEqual true
   }
 }
