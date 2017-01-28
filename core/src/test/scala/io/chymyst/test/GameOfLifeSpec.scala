6479a1a18fdcbf7ddea84fbba281da70db76e4fd
Sergei Winitzki <winitzki@gmail.com>
2017-01-28 00:21:37 -0800

    WIP implemented Game of Life with 1 molecule and 1 reaction, seems to be not working

--- core/src/test/scala/io/chymyst/test/GameOfLifeSpec.scala
+++ core/src/test/scala/io/chymyst/test/GameOfLifeSpec.scala
@@ -1,5 +1,8 @@
 package io.chymyst.test
 
+import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
+
 import io.chymyst.jc._
 import org.scalatest.{FlatSpec, Matchers}
 
@@ -20,50 +23,64 @@ class GameOfLifeSpec extends FlatSpec with Matchers {
   it should "run correctly using single-reaction implementation" in {
     case class Cell(x: Int, y: Int, t: Int, state: Int, label: (Int, Int))
 
-    val cell = m[Cell]
+    val c = m[Cell] // one cell
+    val fc = m[(Int, Int, Int)] // cell state at final time
+    val f = m[(Int, Array[Array[Int]])] // accumulator for cell states at final time
+    val g = b[Unit, Array[Array[Int]]] // blocking molecule to fetch the final accumulator value
 
-    val tp = new FixedPool(4)
+    val tp = new FixedPool(32)
 
-    val boardSize = 5
+    val boardSize = 1
     // Toroidal board of size n * n
-    val maxTimeSteps = 5
+    val maxTimeStep = 1
 
     site(tp)(
+      // One reaction implements the entire Game of Life computation.
       go { case
-        cell(Cell(x0, y0, t0, state0, (0, 0))) +
-          cell(Cell(x1, y1, t1, state1, (1, 0))) +
-          cell(Cell(x2, y2, t2, state2, (-1, 0))) +
-          cell(Cell(x3, y3, t3, state3, (0, 1))) +
-          cell(Cell(x4, y4, t4, state4, (1, 1))) +
-          cell(Cell(x5, y5, t5, state5, (-1, 1))) +
-          cell(Cell(x6, y6, t6, state6, (0, -1))) +
-          cell(Cell(x7, y7, t7, state7, (1, 1))) +
-          cell(Cell(x8, y8, t8, state8, (-1, -1)))
+        c(Cell(x0, y0, t0, state0, (0, 0))) +
+          c(Cell(x1, y1, t1, state1, (1, 0))) +
+          c(Cell(x2, y2, t2, state2, (-1, 0))) +
+          c(Cell(x3, y3, t3, state3, (0, 1))) +
+          c(Cell(x4, y4, t4, state4, (1, 1))) +
+          c(Cell(x5, y5, t5, state5, (-1, 1))) +
+          c(Cell(x6, y6, t6, state6, (0, -1))) +
+          c(Cell(x7, y7, t7, state7, (1, -1))) +
+          c(Cell(x8, y8, t8, state8, (-1, -1)))
         if x0 == (x1 + 1) % boardSize && x0 == (x2 - 1) % boardSize && x0 == x3 && x0 == (x4 + 1) % boardSize && x0 == (x5 - 1) % boardSize && x0 == x6 && x0 == (x7 + 1) % boardSize && x0 == (x8 - 1) % boardSize &&
-          y0 == x1 && y0 == y2 && y0 == (y3 + 1) % boardSize && y0 == (y4 + 1) % boardSize && y0 == (y5 + 1) % boardSize && y0 == (y6 - 1) % boardSize && y0 == (y7 - 1) % boardSize && y0 == (y8 - 1) % boardSize &&
+          y0 == y1 && y0 == y2 && y0 == (y3 + 1) % boardSize && y0 == (y4 + 1) % boardSize && y0 == (y5 + 1) % boardSize && y0 == (y6 - 1) % boardSize && y0 == (y7 - 1) % boardSize && y0 == (y8 - 1) % boardSize &&
           t0 == t1 && t0 == t2 && t0 == t3 && t0 == t4 && t0 == t5 && t0 == t6 && t0 == t7 && t0 == t8 =>
         val newState = getNewState(state0, state1, state2, state3, state4, state5, state6, state7, state8)
-        (-1 to 1).foreach(i => (-1 to 1).foreach(j => cell(Cell(x0, y0, t0 + 1, newState, (i, j)))))
-      }
+        (-1 to 1).foreach(i => (-1 to 1).foreach(j => c(Cell(x0, y0, t0 + 1, newState, (i, j)))))
+        if (t0 == maxTimeStep) fc((x0, y0, newState))
+      },
+      // These reactions are needed to fetch the state of the board at the final time.
+      go { case fc((x, y, state)) + f((count, board)) => board(x)(y) = state; f((count - 1, board)) },
+      go { case g(_, r) + f((0, board)) => r(board) }
     )
 
     val initBoard = Array(
-      Array(0, 0, 0, 0, 0),
+      Array(1, 1, 0, 0, 0),
       Array(0, 1, 1, 0, 0),
-      Array(0, 0, 1, 1, 0),
-      Array(0, 1, 0, 0, 0),
+      Array(1, 0, 0, 0, 0),
+      Array(0, 0, 0, 0, 0),
       Array(0, 0, 0, 0, 0)
     )
 
+    val initTime = LocalDateTime.now
     (0 until boardSize).foreach(x =>
       (0 until boardSize).foreach(y =>
         (-1 to 1).foreach(i =>
           (-1 to 1).foreach(j =>
-            cell(Cell(x, y, 0, initBoard(x)(y), (i, j)))
+            c(Cell(x, y, t = 0, state = initBoard(x)(y), label = (i, j)))
           )
         )
       )
     )
+    f.setLogLevel(2)
+    val finalBoard = g()
+    val elapsed = initTime.until(LocalDateTime.now, ChronoUnit.MILLIS)
+    println(s"Test with board size $boardSize took $elapsed ms. Final board at t=$maxTimeStep")
+    println(finalBoard.map(_.map(x => if (x == 0) "" else "*").mkString(" ")).mkString("\n"))
     tp.shutdownNow()
   }
 
