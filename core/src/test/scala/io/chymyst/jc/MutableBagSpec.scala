e06c642a5ccd19755890a4e9958315c56943be1c
Sergei Winitzki <winitzki@gmail.com>
2017-02-06 14:02:49 -0800

    add more tests for mutable bag

--- core/src/test/scala/io/chymyst/jc/MutableBagSpec.scala
+++ core/src/test/scala/io/chymyst/jc/MutableBagSpec.scala
@@ -13,6 +13,7 @@ class MutableBagSpec extends FlatSpec with Matchers with TimeLimitedTests {
   it should "create empty bag" in {
     val b = new MutableBag[Int, String]
     b.size shouldEqual 0
+    b.getCount(1) shouldEqual 0
     b.getOne(1) shouldEqual None
   }
 
@@ -21,12 +22,16 @@ class MutableBagSpec extends FlatSpec with Matchers with TimeLimitedTests {
     b.addToBag(1, "a")
     b.size shouldEqual 1
     b.getOne(1) shouldEqual Some("a")
+    b.getCount(1) shouldEqual 1
+    b.getCount(2) shouldEqual 0
   }
 
   it should "make a bag with one element" in {
     val b = MutableBag.of(1, "a")
     b.size shouldEqual 1
     b.getOne(1) shouldEqual Some("a")
+    b.getCount(1) shouldEqual 1
+    b.getCount(2) shouldEqual 0
   }
 
   it should "print a bag" in {
@@ -41,8 +46,13 @@ class MutableBagSpec extends FlatSpec with Matchers with TimeLimitedTests {
     b.size shouldEqual 1
     b.addToBag(1, "a")
     b.size shouldEqual 2
+    b.getCount(1) shouldEqual 2
+    b.getCount(2) shouldEqual 0
+
     b.getOne(1) shouldEqual Some("a")
     b.removeFromBag(1, "a")
+    b.getCount(1) shouldEqual 1
+
     b.size shouldEqual 1
     b.getOne(1) shouldEqual Some("a")
     b.removeFromBag(1, "a")
@@ -54,14 +64,21 @@ class MutableBagSpec extends FlatSpec with Matchers with TimeLimitedTests {
     b.size shouldEqual 1
     b.addToBag(1, "b")
     b.size shouldEqual 2
+    b.getCount(1) shouldEqual 2
+    b.getCount(2) shouldEqual 0
+
     val c1 = b.getOne(1).get
     Set("a", "b").contains(c1) shouldEqual true
     b.removeFromBag(1, "a")
+    b.getCount(1) shouldEqual 1
+
     val c2 = b.getOne(1)
     c2 shouldEqual Some("b")
     b.size shouldEqual 1
     b.removeFromBag(1, "b")
     b.size shouldEqual 0
+    b.getCount(1) shouldEqual 0
+
   }
   it should "add two elements with different keys and different values, then remove them one by one" in {
     val b = new MutableBag[Int, String]
@@ -69,10 +86,17 @@ class MutableBagSpec extends FlatSpec with Matchers with TimeLimitedTests {
     b.size shouldEqual 1
     b.addToBag(2, "b")
     b.size shouldEqual 2
+    b.getCount(1) shouldEqual 1
+    b.getCount(2) shouldEqual 1
+
     b.getOne(1) shouldEqual Some("a")
     b.getOne(2) shouldEqual Some("b")
+
     b.removeFromBag(1, "a")
     b.size shouldEqual 1
+    b.getCount(1) shouldEqual 0
+    b.getCount(2) shouldEqual 1
+
     b.getOne(1) shouldEqual None
     b.getOne(2) shouldEqual Some("b")
     b.removeFromBag(2, "b")
