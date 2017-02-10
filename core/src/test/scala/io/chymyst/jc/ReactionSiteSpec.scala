e1d07b1166df7ab22091e8a287f96d74e2fbfae0
Sergei Winitzki <winitzki@gmail.com>
2017-02-09 17:11:30 -0800

    pipelined detection is not quite correct; add failing tests

--- core/src/test/scala/io/chymyst/jc/ReactionSiteSpec.scala
+++ core/src/test/scala/io/chymyst/jc/ReactionSiteSpec.scala
@@ -166,7 +166,7 @@ class ReactionSiteSpec extends FlatSpec with Matchers with BeforeAndAfterEach {
 
     val x = 10
 
-    val result = withPool(new FixedPool(2)) { tp =>
+    withPool(new FixedPool(2)) { tp =>
       site(tp)(
         go { case f(_, r) =>
           if (x > 0) throw new Exception("crash! ignore this exception")
@@ -177,10 +177,7 @@ class ReactionSiteSpec extends FlatSpec with Matchers with BeforeAndAfterEach {
         f.timeout()(1.seconds)
       }
       thrown.getMessage shouldEqual "Error: In Site{f/B => ...}: Reaction {f/B(_) => } with inputs [f/B()] finished without replying to f/B. Reported error: crash! ignore this exception"
-    }
-    if (result.isFailure) println(s"Test failed with message: ${result.failed.get.getMessage}")
-    result.get shouldEqual Succeeded
-    result.isFailure shouldEqual false
+    }.get
   }
 
   behavior of "pipeline molecule detection"
@@ -208,31 +205,159 @@ class ReactionSiteSpec extends FlatSpec with Matchers with BeforeAndAfterEach {
     Seq(a, c1, c2, c3, c4).map(_.isPipelined) shouldEqual Seq.fill(5)(true)
   }
 
-  it should "work correctly for reactions with conditions" in {
+  it should "work correctly for reaction with a simple condition and two inputs" in {
+    val a = m[Int]
+    val c = m[Int]
+    val d = m[Option[Int]]
+    val e = m[(Int, Int)]
+    site(
+      go { case a(x) + c(y) if x > 0 => }
+    )
+    site(
+      go { case d(Some(x)) + e((_, 2)) => }
+    )
+    checkExpectedPipelined(Map(a -> true, c -> true, d -> true, e -> true))
+  }
+
+  it should "work correctly for reactions with complicated conditions" in {
+    val a = m[Int]
+    val c = m[(Int, Int)]
+    val d = m[Int]
+    val e = m[Int]
+    val f = m[Int]
+    val g = m[(Int, Int)]
+
+    site(
+      go { case a(x) if x > 0 => },
+      go { case a(x) + c((y, z)) if x > 0 && y > z => }, // same condition for all reactions; separable guard; so both a and c are pipelined
+
+      go { case d(x) + e(_) => },
+      go { case d(x) + f(y) if y > 0 => }, // d, e, f should be pipelined
+
+      go { case g((y, z)) if y > z => } // g is pipelined
+
+    )
+    checkExpectedPipelined(Map(e -> true, f -> true, g -> true, c -> true, a -> true, d -> true))
+  }
+
+  it should "prevent pipelining for reactions with cross-molecule guards" in {
+    val a = m[Int]
+    val c = m[(Int, Int)]
+    val d = m[Int]
+    val e = m[Int]
+    val f = m[Int]
+    val g = m[Int]
+    val h = m[(Int, Int)]
+    val j = m[Int]
+    val k = m[Int]
+
+    site(
+      go { case a(x) + c((y, z)) if y > z && z > 0 && x > 0 => }, // no cross-molecule guard here
+
+      go { case d(x) + e(y) + e(z) if x > 0 && y > 0 => }, // d should be pipelined, but not e since the repeated molecule has a cross-condition
+
+      go { case f(x) + g(y) if x > 0 && y > x => }, // f, g are not pipelined due to cross-molecule guard
+
+      go { case h((y, z)) + j(t) + k(x) if t > 0 && z > x => } // cross-molecule guard prevents both h and k from being pipelined, but not j
+    )
+    checkExpectedPipelined(Map(a -> true, c -> true, d -> true, e -> false, f -> false, g -> false, h -> false, j -> true, k -> false))
+  }
+
+  def checkExpectedPipelined(expectedMap: Map[Molecule, Boolean]) = {
+    val transformed = expectedMap.toList.map { case (t, r) => (t, t.isPipelined, r) }
+    // Print detailed message.
+    val difference = transformed.filterNot { case (_, a, b) => a == b }.map { case (m, actual, expected) => s"$m.isPipelined is $actual instead of $expected" }
+    if (difference.nonEmpty) println(s"Test fails: ${difference.mkString("; ")}")
+    val (left, right) = (transformed.map(_._2), transformed.map(_._3))
+    left shouldEqual right
+  }
+
+  it should "not pipeline reactions with non-factorizable conditions" in {
+    val c1 = m[Int]
+    val c3 = m[Int]
+    val c4 = m[Int]
+    val c5 = m[Int]
+
+    site(
+      go { case c1(x) if x > 0 => }, // c1 is pipelined:
+      go { case c1(x) + c1(y) if x < 0 => }, // simple condition but the molecule is repeated, which creates a cross-molecule dependency
+
+      go { case c3(x) + c4(y) => },
+      go { case c3(x) + c5(y) if y > 0 => } // other inputs, but no conditions
+
+    )
+    checkExpectedPipelined(Map(c1 -> false, c3 -> true, c4 -> true, c5 -> true))
+  }
+
+  it should "work correctly for reactions with factorizable conditions" in {
     val c1 = m[Int]
     val c2 = m[Int]
     val c3 = m[Int]
     val c4 = m[Int]
-    val d1 = m[(Int, Int)]
-    val d2 = m[(Int, Int)]
+    val c5 = m[Int]
 
     site(
-      go { case c1(x) if x > 0 => },
-      go { case c1(x) if x < 0 => },
-      go { case c1(x) + c1(_) => },
+      go { case c1(x) if x > 0 => }, // c1 is pipelined:
+      go { case c1(x) if x < 0 => }, // simple condition
+      go { case c1(x) + c1(_) => }, // unconditional with no other inputs - still ok to pipeline c1
 
       go { case c2(x) if x > 0 => },
-      go { case c2(x) if x < 0 => },
-      go { case c2(x) + c3(y) => },
+      go { case c2(x) if x < 0 => }, // simple condition
+      go { case c2(x) + c3(y) => }, // unconditional but has other inputs, so c2 cannot be pipelined (but c3 can be)
 
       go { case c4(x) if x > 0 => },
-      go { case c4(x) + d1((y,z)) => },
-      
-      go { case d2((y,z)) if y > z => }
+      go { case c4(x) + c5(y) => } // other inputs and a condition prevents pipelining of c4
+
     )
+    checkExpectedPipelined(Map(c1 -> true, c2 -> false, c3 -> true, c4 -> false, c5 -> true))
+  }
 
-    Seq(c1, c2, c3, c4, d1, d2).map(_.isPipelined) shouldEqual Seq(true, false, true, false, true, true)
+  it should "detect pipelining while also detecting livelock" in {
+    val a = m[Unit]
+    val c = m[Int]
+    intercept[Exception] {
+      site(
+        go { case c(x) => },
+        go { case c(x) + a(_) => } // this is a livelock but we are interested in pipelining of c
+      )
+    }.getMessage shouldEqual "In Site{a + c => ...; c => ...}: Unavoidable nondeterminism: reaction {a(_) + c(x) => } is shadowed by {c(x) => }"
+    checkExpectedPipelined(Map(c -> true, a -> true))
+  }
 
+  it should "detect pipelining while also detecting nondeterminism" in {
+    val c = m[Int]
+    intercept[Exception] {
+      site(
+        go { case c(x) if x > 0 => },
+        go { case c(x) + c(y) => },
+        go { case c(x) + c(y) + c(z) => } // unconditional nondeterminism with several inputs
+      )
+    }.getMessage shouldEqual "In Site{c + c + c => ...; c + c => ...; c => ...}: Unavoidable nondeterminism: reaction {c(x) + c(y) + c(z) => } is shadowed by {c(x) + c(y) => }"
+    checkExpectedPipelined(Map(c -> true))
+  }
+
+
+  it should "detect pipelining while also detecting livelock, with condition" in {
+    val a = m[Unit]
+    val c = m[Int]
+    //    intercept[Exception] {
+    site(
+      go { case c(x) if x > 0 => },
+      go { case c(x) + a(_) if x > 0 => } // this is a livelock (although we can't detect it!) but we are interested in pipelining of c
+    )
+    //    }.getMessage shouldEqual "In Site{a + c => ...; c => ...}: Unavoidable nondeterminism: reaction {a(_) + c(x) => } is shadowed by {c(x) => }"
+    checkExpectedPipelined(Map(c -> true, a -> true))
+  }
+
+  it should "detect pipelining with two conditions" in {
+    val a = m[Int]
+    val c = m[Int]
+
+    site(
+      go { case a(x) if x > 0 => },
+      go { case a(x) + c(y) if x > 0 => } // same condition for all reactions, so a is pipelined
+    )
+    checkExpectedPipelined(Map(c -> true, a -> true))
   }
 
 }
