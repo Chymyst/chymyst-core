a7438f4e2592c61e8153f8093c90af3d9c86dc1d
Sergei Winitzki <winitzki@gmail.com>
2017-06-19 00:00:58 -0700

    additional tests for pipelined and non-pipelined reactions

--- benchmark/src/test/scala/io/chymyst/benchmark/ConcurrentCounterSpec.scala
+++ benchmark/src/test/scala/io/chymyst/benchmark/ConcurrentCounterSpec.scala
@@ -8,112 +8,95 @@ import io.chymyst.test.LogSpec
 
 class ConcurrentCounterSpec extends LogSpec {
 
-  val count = 10000
+  val count = 50000
+
+  def runBenchmark(count: Int, threads: Int, verbose: Boolean, writeFile: Boolean = false): Unit = {
+    val c = m[Int]
+    val g = b[Unit, Int]
+    val i = m[Unit]
+    val d = m[Unit]
+    val f = b[Long, Long]
+
+    val memoryLogger = new MemoryLogger
+    val elapsed = withPool(FixedPool(threads).withReporter(if (verbose) new DebugAllReporter(memoryLogger) else ConsoleEmptyReporter)) { tp ⇒
+
+      site(tp)(
+        go { case c(0) + f(tInit, r) ⇒
+          val x = System.nanoTime()
+          r(x - tInit)
+        },
+        go { case g(_, reply) + c(n) ⇒ c(n); reply(n) },
+        go { case c(n) + i(_) ⇒ c(n + 1) },
+        go { case c(n) + d(x) if n > 0 ⇒ c(n - 1) }
+      )
+      c(count)
+      val initialTime = System.nanoTime()
+      (1 to count).foreach { _ => d() }
+      f(initialTime)
+    }.get
+    println(s"benchmark with $count reactions on $threads threads, with${if(verbose) " " + memoryLogger.messages.size else " no"} logs took ${formatNanosToMs(elapsed)}")
+    if (writeFile) write.over(pwd / 'logs / s"benchmark_$count-on-$threads-threads_${if(verbose) "" else "no-"}logging.txt", memoryLogger.messages.map(_ + "\n"))
+  }
+
+  def runPipelinedBenchmark(count: Int, threads: Int, pipelined: Boolean, verbose: Boolean, writeFile: Boolean = false): Unit = {
+    val c = m[Int]
+    val d = m[Int]
+    val i = m[Unit]
+    val done = m[Unit]
+    val f = b[Long, Long]
+
+    val memoryLogger = new MemoryLogger
+    val elapsed = withPool(FixedPool(threads).withReporter(if (verbose) new DebugAllReporter(memoryLogger) else ConsoleEmptyReporter)) { tp ⇒
+      site(tp)(
+        go { case done(_) + f(tInit, r) ⇒
+          val x = System.nanoTime()
+          r(x - tInit)
+        },
+        go { case c(n) + i(_) ⇒ c(n + 1) },
+        if (pipelined) go { case c(n) + d(x) ⇒ if (n > 1) c(n - 1) else done() } else go { case c(n) + d(x) if n + x > -1 ⇒ if (n > 1) c(n - 1) else done() }
+      )
+      val initialTime = System.nanoTime()
+      c(count)
+      (1 to count).foreach(d)
+      f(initialTime)
+    }.get
+
+    println(s"${if(pipelined) "" else "non-"}pipelined benchmark with $count reactions on $threads threads, with${if(verbose) " " + memoryLogger.messages.size else " no"} logs took ${formatNanosToMs(elapsed)}")
+    if (writeFile) write.over(pwd / 'logs / s"${if(pipelined) "" else "non-"}pipelined benchmark_$count-on-$threads-threads_${if(verbose) "" else "no-"}logging.txt", memoryLogger.messages.map(_ + "\n"))
+  }
 
   behavior of "concurrent counter"
 
-  it should s"produce no-logging benchmark1 data on $count counter runs" in {
-    val c = m[Int]
-    val g = b[Unit, Int]
-    val i = m[Unit]
-    val d = m[Unit]
-    val f = b[Long, Long]
-
-    val elapsed = withPool(FixedPool(8)) { tp ⇒
-      site(tp)(
-        go { case c(0) + f(tInit, r) =>
-          val x = System.nanoTime()
-          r(x - tInit)
-        },
-        go { case g(_, reply) + c(n) => c(n); reply(n) },
-        go { case c(n) + i(_) => c(n + 1) },
-        go { case c(n) + d(_) if n > 0 => c(n - 1) }
-      )
-
-      c(count)
-      val initialTime = System.nanoTime()
-      (1 to count).foreach { _ => d() }
-      f(initialTime)
-    }.get
-    println(s"benchmark1 without logging took ${formatNanosToMs(elapsed)}")
+  it should s"produce no-logging benchmark1 data on $count counter runs with 8 threads" in {
+    runBenchmark(count, 8, verbose = false)
   }
 
-  it should s"produce benchmark1 data on $count counter runs" in {
-    val c = m[Int]
-    val g = b[Unit, Int]
-    val i = m[Unit]
-    val d = m[Unit]
-    val f = b[Long, Long]
-
-    val memoryLogger = new MemoryLogger
-    val elapsed = withPool(FixedPool(8).withReporter(new DebugAllReporter(memoryLogger))) { tp ⇒
-      site(tp)(
-        go { case c(0) + f(tInit, r) =>
-          val x = System.nanoTime()
-          r(x - tInit)
-        },
-        go { case g(_, reply) + c(n) => c(n); reply(n) },
-        go { case c(n) + i(_) => c(n + 1) },
-        go { case c(n) + d(_) if n > 0 => c(n - 1) }
-      )
-
-      c(count)
-      val initialTime = System.nanoTime()
-      (1 to count).foreach { _ => d() }
-      f(initialTime)
-    }.get
-    println(s"benchmark1 with logging took ${formatNanosToMs(elapsed)}")
-//    write.over(pwd / 'logs / "benchmark1_logging.txt", memoryLogger.messages.map(_ + "\n"))
+  it should s"produce logging benchmark1 data on $count counter runs with 8 threads" in {
+    runBenchmark(count, 8, verbose = true)
   }
 
-  it should s"produce no-logging pipelined benchmark1 data on $count counter runs" in {
-    val c = m[Int]
-    val d = m[Unit]
-    val done = m[Unit]
-    val f = b[Long, Long]
-
-    val elapsed = withPool(FixedPool(8)) { tp ⇒
-      site(tp)(
-        go { case done(_) + f(tInit, r) ⇒
-          val x = System.nanoTime()
-          r(x - tInit)
-        },
-        go { case c(n) + d(_) ⇒ if (n > 1) c(n - 1) else done() }
-      )
-
-      c(count)
-      val initialTime = System.nanoTime()
-      (1 to count).foreach { _ => d() }
-      f(initialTime)
-    }.get
-
-    println(s"pipelined benchmark1 without logging took ${formatNanosToMs(elapsed)}")
+  it should s"produce no-logging pipelined benchmark1 data on $count counter runs with 8 threads" in {
+    runPipelinedBenchmark(count, 8, pipelined = true, verbose = false)
   }
 
-  it should s"produce pipelined benchmark1 data on $count counter runs" in {
-    val c = m[Int]
-    val d = m[Unit]
-    val done = m[Unit]
-    val f = b[Long, Long]
+  // This is extremely slow due to quadratic time while printing log messages: the pipelined queue needs to be traversed each time!
+//  it should s"produce logging pipelined benchmark1 data on $count counter runs with 8 threads" in {
+//    runPipelinedBenchmark(count, 8, pipelined = true, verbose = true)
+//  }
 
-    val memoryLogger = new MemoryLogger
-    val elapsed = withPool(FixedPool(8).withReporter(new DebugAllReporter(memoryLogger))) { tp ⇒
-      site(tp)(
-        go { case done(_) + f(tInit, r) ⇒
-          val x = System.nanoTime()
-          r(x - tInit)
-        },
-        go { case c(n) + d(_) ⇒ if (n > 1) c(n - 1) else done() }
-      )
-
-      c(count)
-      val initialTime = System.nanoTime()
-      (1 to count).foreach { _ => d() }
-      f(initialTime)
-    }.get
-
-    println(s"pipelined benchmark1 with logging took ${formatNanosToMs(elapsed)}")
-//    write.over(pwd / 'logs / "benchmark1_pipelined_logging.txt", memoryLogger.messages.map(_ + "\n"))
+  it should s"produce no-logging non-pipelined benchmark1 data on $count counter runs with 8 threads" in {
+    runPipelinedBenchmark(count, 8, pipelined = false, verbose = false)
   }
 
+  it should s"produce no-logging benchmark1 data on $count counter runs with 1 threads" in {
+    runBenchmark(count, 1, verbose = false)
+  }
+
+  it should s"produce no-logging pipelined benchmark1 data on $count counter runs with 1 threads" in {
+    runPipelinedBenchmark(count, 1, pipelined = true, verbose = false)
+  }
+
+  it should s"produce no-logging non-pipelined benchmark1 data on $count counter runs with 1 threads" in {
+    runPipelinedBenchmark(count, 1, pipelined = false, verbose = false)
+  }
 }
